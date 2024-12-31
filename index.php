9f2533936e76954909f040a212a38f9a65d7ca1f
BriannaLooney <briannaelizabethlooney@hotmail.com>
2024-12-30 17:42:45 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+    <form method="get" action="greet_user.php">
+    Name
+        <input type="text" name="name">
+        <br>
+        <input type="text" name="email">
+        <br>
+         <input type="text" name="Message">
+        <br>
+         <input type="submit" name="Submit">
+    </form>
+    
+    <a href="index.php">reset</a>
+    </body>
+    </html>
