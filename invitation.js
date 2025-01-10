b7a46a96cbf105b10b68608a7c4c098759713ae6
BriannaLooney <briannaelizabethlooney@hotmail.com>
2025-01-10 15:16:31 -0800

    Update invitation.js

--- invitation.js
+++ invitation.js
@@ -11,3 +11,27 @@ function replaceContent() {
 	//setting the HTML code in the span id recipientNamePlaceholder with the variable 
 	document.getElementById("recipientNamePlaceholder").innerHTML = myRecipientName;
 } 
+// Preload images
+const images = [
+    'images/banner1.jpg',
+    'images/banner2.jpg',
+    'images/banner3.jpg'
+];
+
+const preloadedImages = [];
+images.forEach((image) => {
+    const img = new Image();
+    img.src = image;
+    preloadedImages.push(img);
+});
+
+// Cycling banner
+let currentIndex = 0;
+const bannerImage = document.getElementById('banner-image');
+
+function cycleBanner() {
+    currentIndex = (currentIndex + 1) % images.length;
+    bannerImage.src = images[currentIndex];
+}
+
+setInterval(cycleBanner, 3000); // Change image every 3 seconds
