ce78d48e774d65f68f1345e50b2f6824122acd00
BriannaLooney <briannaelizabethlooney@hotmail.com>
2025-01-10 23:47:16 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-// Preload images
-const images = [
-    'images/banner1.jpg',
-    'images/banner2.jpg',
-    'images/banner3.jpg'
-];
 
-const = Preloading = [];
-images.forEach((image) => {
-    const img = new Image();
-    img.src = image;
-   Preloading.push(img);
-});
 
 // Cycling banner
 let currentIndex = 0;
