95c9c4a49f7bd5f6056ba93680d3cf0d4a064d54
BriannaLooney <briannaelizabethlooney@hotmail.com>
2025-01-10 20:22:45 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+// Preload images
+const images = [
+    'images/banner1.jpg',
+    'images/banner2.jpg',
+    'images/banner3.jpg'
+];
+
+const =Preloading = [];
+images.forEach((image) => {
+    const img = new Image();
+    img.src = image;
+    Preloading.push(img);
+});
+
+// Cycling banner
+let currentIndex = 0;
+const bannerImage = document.getElementById('banner-image');
+
+function cycleBanner() {
+    currentIndex = (currentIndex + 1) % images.length;
+    bannerImage.src = images[currentIndex];
+}
+
+setInterval(cycleBanner, 3000); // Change image every 3 seconds
